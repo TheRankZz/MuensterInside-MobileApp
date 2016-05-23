bb2e8f830782a440992463355fe907753745e61c
Juli1995 <julebracht@web.de>
2016-05-23 17:29:35 +0200

    ShowCommentActivity lädt die Daten aus der MockKlasse

--- app/src/main/java/de/muensterinside/mobile/ShowCommentActivity.java
+++ app/src/main/java/de/muensterinside/mobile/ShowCommentActivity.java
@@ -4,24 +4,50 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.muensterinside.mobile.entities.Comment;
+
 
 public class ShowCommentActivity extends AppCompatActivity {
 
-    private  TextView Textv;
+
+    MuensterInsideAndroidApplication myApp;
+    List<Comment> comments;
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_comment);
+        myApp = (MuensterInsideAndroidApplication) getApplication();
 
-        Textv = (TextView)findViewById(R.id.textView);
-        Intent iin = getIntent();
-        Bundle b = iin.getExtras();
 
-        String j = (String) b.get("name");
-        Textv.setText(j);
+        listView = (ListView) findViewById(R.id.listView);
+
+        // Der Webservice wird aufgerufen und alle Categories werden in eine Liste gespeichert
+        try {
+            comments = myApp.getMuensterInsideMobile().getCommentsByLocation(1);
+            Intent myIntent = getIntent();
+
+            List myList = new ArrayList<String>();
+            for(int i=0; i < comments.size(); i++){
+                myList.add(comments.get(i).getText());
+            }
+            // Es wird ein Adapter erstellt der die listView mit einträgen befüllt
+            ArrayAdapter<String> adapter;
+            adapter=new ArrayAdapter<String>(this, R.layout.content_item_list_category, myList);
+            listView.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }
+        catch(Exception e){e.printStackTrace();}
 
 
         Button button = (Button) findViewById(R.id.buttonZuerueck);
