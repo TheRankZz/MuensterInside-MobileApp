c94146d1afaf1ad0fd3820d01a996c21ed70aa26
Juli1995 <julebracht@web.de>
2016-06-16 12:52:55 +0200

    Username anzeigen in der Einstellung

--- app/src/main/java/de/muensterinside/mobile/PrefsActivity.java
+++ app/src/main/java/de/muensterinside/mobile/PrefsActivity.java
@@ -1,9 +1,14 @@
 package de.muensterinside.mobile;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 
 /**
@@ -13,12 +18,18 @@ import android.util.Log;
 
 public class PrefsActivity extends PreferenceActivity {
     public static final String TAG = "PrefsActivity";
+    EditTextPreference usernameEditTextPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate() gestartet");
         super.onCreate(savedInstanceState);
-        this.addPreferencesFromResource(R.xml.prefs);
+        addPreferencesFromResource(R.xml.prefs);
+        usernameEditTextPreference = (EditTextPreference)findPreference("username");
+
+
+        SharedPreferences getUsername = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+        usernameEditTextPreference.setText(getUsername.getString("username", "0"));
 
     }
 
